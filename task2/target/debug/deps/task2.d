4e056346e12886a5b79b7b280b14c38c443b3dfe
B-Mustansir <mustansirzain2@gmail.com>
2023-04-08 21:42:32 +0530

    Added task2

---
+++ task2/target/debug/deps/task2.d
@@ -0,0 +1,5 @@
+C:\Users\musta\OneDrive\Documents\GitHub\Quark_Rust\task2\target\debug\deps\task2.exe: src\main.rs
+
+C:\Users\musta\OneDrive\Documents\GitHub\Quark_Rust\task2\target\debug\deps\task2.d: src\main.rs
+
+src\main.rs:
---
+++ task2/target/debug/task2.d
@@ -0,0 +1,1 @@
+C:\Users\musta\OneDrive\Documents\GitHub\Quark_Rust\task2\target\debug\task2.exe: C:\Users\musta\OneDrive\Documents\GitHub\Quark_Rust\task2\src\main.rs
